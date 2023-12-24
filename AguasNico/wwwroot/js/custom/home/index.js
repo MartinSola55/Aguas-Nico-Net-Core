5d7af33d266ffa2449ed7d7ca25cfbea397f7d41
Martin Sola <martinrsola55@gmail.com>
2023-12-24 19:13:48 -0300

    Improve table loading in home page

--- AguasNico/wwwroot/js/custom/home/index.js
+++ AguasNico/wwwroot/js/custom/home/index.js
@@ -1,5 +1,15 @@
 
 $(document).ready(function () {
+    $('#routesTable').DataTable({
+        "order": false,
+        "info": false,
+        "paging": false,
+        "searching": false,
+        "language": {
+            "emptyTable": "No hay planillas para mostrar",
+            "zeroRecords": "No hay planillas para mostrar"
+        },
+    });
     $('#DatePicker').bootstrapMaterialDatePicker({
         maxDate: new Date(),
         time: false,
@@ -16,30 +26,47 @@ $(document).ready(function () {
     });
 
     function salesDay(selectedDate) {
-        $('#routesTable tbody').html('');
+        $('#routesTable').DataTable().clear().draw();
+        let loadingRow = `<tr>
+            <td>
+                <div class="spinner-border text-primary" role="status">
+                    <span class="sr-only">Cargando...</span>
+                </div>
+            </td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>`;
+        $('#routesTable').DataTable().rows.add($(loadingRow)).draw();
         $('#form-searchRoutesByDate input[name="dateString"]').val(selectedDate);
         let form = $('#form-searchRoutesByDate');
         $.ajax({
             url: $(form).attr('action'),
             method: $(form).attr('method'),
             data: $(form).serialize(),
-            success: function (response) { //id, dealer, totalCarts, completedCarts, state, collected
-                const row = response.routes.map(route =>`
-                    <tr class="clickable" data-url="/route/details">
+            success: function (response) {
+                $('#routesTable').DataTable().clear().draw();
+                response.routes.forEach(route => {
+                    let row = `<tr class="clickable" data-url="/route/details">
                         <td><h6>${route.dealer}</h6></td>
                         <td>${route.completedCarts}/${route.totalCarts}</td>
-                        ${route.state == 'Pendiente' ? `
-                        <td><span class="label label-warning">${route.state}</span></td>`
-                        : `
-                        <td><span class="label label-success">${route.state}</span></td>`}
+                        ${route.state == 'Pendiente' ? `<td><span class="label label-warning">${route.state}</span></td>` : `<td><span class="label label-success">${route.state}</span></td>`}
                         <td>$ ${route.collected}</td>
-                    </tr>
-                `).join('');
-                $('#routesTable tbody').append(row);
+                    </tr>`;
+                    $('#routesTable').DataTable().rows.add($(row)).draw();
+                });
             },
             error: function (error) {
-                // Maneja los errores aquí
-                console.log('Error:', error);
+                $('#routesTable').DataTable().clear().draw();
+                let row = `<tr>
+                    <td>
+                        <h6 class="text-danger">No se pudo cargar la información</h6>
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>`;
+                $('#routesTable').DataTable().rows.add($(row)).draw();
             }
         });
     }
