240d382409867bd117eb5f1d4b25e30f864bbfea
Martín Sola <msola@frro.utn.edu.ar>
2024-04-04 20:45:36 -0300

    Search clients by name and id in manual cart

--- AguasNico/wwwroot/js/custom/routes/manualCart.js
+++ AguasNico/wwwroot/js/custom/routes/manualCart.js
@@ -2,6 +2,7 @@ function openModal(clientID, clientName) {
     $("#form-searchClientProducts input[name='id']").val(clientID);
     $("#form-confirmCart input[name='Cart.ClientID']").val(clientID);
     $("#form-confirmCart input:not([type='hidden']").val("");
+    $("#cartPaymentAmount").val("");
     $("#divClientAbonos").hide();
 
     $(".modal-title").text(`Confirmar bajada para ${clientName}`);
@@ -184,6 +185,115 @@ function confirmCart() {
     });
 }
 
+function searchByName() {
+    const name = $('#clientName').val();
+
+    if (name.length < 4)
+        return toastrWarning("Debes ingresar al menos 4 caracteres para buscar por nombre");
+
+    loadingRow();
+    const form = $('#form-searchByName');
+    $.ajax({
+        url: $(form).attr('action'),
+        method: $(form).attr('method'),
+        data: $(form).serialize(),
+        success: function (response) {
+            if (response.data.length === 0) {
+                emptyTable();
+                return;
+            }
+            fillTable(response.data)
+        },
+        error: function () {
+            errorRow();
+        }
+    });
+}
+
+function searchByID() {
+    const id = $('#clientID').val();
+
+    loadingRow();
+    const form = $('#form-searchByID');
+    $.ajax({
+        url: $(form).attr('action'),
+        method: $(form).attr('method'),
+        data: $(form).serialize(),
+        success: function (response) {
+            if (response.data.length === 0) {
+                emptyTable();
+                return;
+            }
+            fillTable(response.data)
+        },
+        error: function () {
+            errorRow();
+        }
+    });
+}
+
+function loadingRow() {
+    $('#DataTable tbody').empty();
+    let loadingRow = `
+    <tr>
+        <td>
+            <div class="spinner-border text-primary" role="status">
+                <span class="sr-only">Cargando...</span>
+            </div>
+        </td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>`;
+    $('#DataTable tbody').append($(loadingRow));
+}
+
+function errorRow() {
+    $('#DataTable tbody').empty();
+    let row = `
+    <tr>
+        <td><h6 class="text-danger">No hay clientes que coincidan con la búsqueda</h6></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>`;
+    $('#DataTable tbody').append($(row));
+}
+
+function fillTable(clients) {
+    $('#DataTable tbody').empty();
+    clients.forEach(item => {
+        let row = `
+            <tr data-id="${item.id}">
+                <td>${item.name}</td>
+                <td>${item.address}</td>
+                <td>$${item.debt}</td>
+                <td>${item.dealer}</td>
+                <td>
+                    <div class="d-flex justify-content-center">
+                        <button class="btn btn-info btn-rounded float-right" onclick='openModal(${item.id}, "${item.name}")'>Seleccionar</button>
+                    </div>
+                </td>
+            </tr>`;
+        $('#DataTable tbody').append($(row));
+    });
+}
+
+function emptyTable() {
+    $('#DataTable tbody').empty();
+    let row = `
+    <tr>
+        <td colspan="5"><h6>No hay clientes que coincidan con la búsqueda</h6></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>`;
+    $('#DataTable tbody').append($(row));
+}
+
 $(document).ready(function () {
     $('#DataTable').DataTable({
         "order": false,
@@ -201,5 +311,17 @@ $(document).ready(function () {
                 "sPrevious": "Anterior",
             },
         },
+    });
+
+    let timeoutName;
+    $('#clientName').on('input', function () {
+        clearTimeout(timeoutName);
+        timeoutName = setTimeout(searchByName, 1000);
+    });
+
+    let timeoutId;
+    $('#clientID').on('input', function () {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(searchByID, 1000);
     });
 });
